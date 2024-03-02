fbd2c39adb2430d13f2c95f1a97caebac990e2cd
OktayCavus <oktaycvs61@gmail.com>
2024-03-02 16:27:12 +0300

    page.php deki row footer ile çakıştığı için farklı şekilde isimlendirildi

--- page.php
+++ page.php
@@ -1,4 +1,7 @@
-<?php require_once 'inc/header.php'; ?>
+<?php
+define('security', true);
+
+require_once 'inc/header.php'; ?>
 
 <!-- WRAPPER START -->
 <div class="wrapper bg-dark-white">
@@ -8,39 +11,38 @@
 	<!-- HEADER-AREA END -->
 	<!-- Mobile-menu start -->
 	<?php require_once 'inc/mobilmenu.php'; ?>
+
+
 	<?php
 	$sef = get('pagesef');
 	if (!$sef) {
 		go(site);
 	}
 
-	$page = $db->prepare('SELECT * FROM sayfalar WHERE sef = :s AND durum = :d');
-	$page->execute([
-		':s' => $sef,
-		':d' => 1,
-	]);
+	$page = $db->prepare("SELECT * FROM sayfalar WHERE sef=:s AND durum=:d");
+	$page->execute([':s' => $sef, ':d' => 1]);
 	if ($page->rowCount()) {
-		$row = $page->fetch(PDO::FETCH_OBJ);
+		$roww = $page->fetch(PDO::FETCH_OBJ);
 	} else {
-		go(site);
+		go(site . '/404.php');
 		exit;
 	}
 	?>
 
 	<!-- Mobile-menu end -->
 	<!-- HEADING-BANNER START -->
-	<div class="heading-banner-area overlay-bg" style="    background: rgba(0, 0, 0, 0) url(<?php echo site; ?>/uploads/<?php echo $row->kapak; ?>) no-repeat scroll center center / cover;">
+	<div class="heading-banner-area overlay-bg" style="background: rgba(0, 0, 0, 0) url(<?php echo site; ?>/uploads/<?php echo $roww->kapak; ?>) no-repeat scroll center center / cover;">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
 					<div class="heading-banner">
 						<div class="heading-banner-title">
-							<h2><?php echo $row->baslik; ?></h2>
+							<h2><?php echo $roww->baslik; ?></h2>
 						</div>
 						<div class="breadcumbs pb-15">
 							<ul>
 								<li><a href="<?php echo site; ?>">Ana Sayfa</a></li>
-								<li><?php echo $row->baslik; ?></li>
+								<li><?php echo $roww->baslik; ?></li>
 							</ul>
 						</div>
 					</div>
@@ -57,9 +59,8 @@
 
 					<div class="col-lg-12">
 
-						<h4 class="title-1 title-border text-uppercase mb-30"><?php echo $row->baslik; ?></h4>
-						<p><?php echo $row->icerik; ?></p>
-
+						<h4 class="title-1 title-border mb-30"><?php echo $roww->baslik; ?></h4>
+						<?php echo $roww->icerik; ?>
 
 					</div>
 				</div>
@@ -68,6 +69,7 @@
 	</div>
 	<!-- ABOUT-US-AREA END -->
 	<!-- TEAM-MEMBER-AREA END -->
+
 	<!-- TEAM-MEMBER-AREA END -->
 	<!-- FOOTER START -->
 	<?php require_once 'inc/footer.php'; ?>
